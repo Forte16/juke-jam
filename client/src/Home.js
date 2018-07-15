abd4ad21313c9e6c6ba9a4b4c0fb0df63e375b80
Alexandro Forte <forte.alexandro@gmail.com>
2018-07-15 17:21:49 -0400

    refactor Home.js

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './css/Home.css';
 
-class Home extends Component {
-  render() {
-    return (
-      <div className="main">
-        <div className="title"> Juke Jam </div>
-        <div className="about">
-          Juke Jam is a web application that allows users to easily receive recommendations for music at social events, and effortlessly add the suggestions to their party playlist.
-        </div>
-        <div className="center">
-          <a href="/about" className="goToWebsite"> Click here to learn more. </a>
-        </div>
-
-          <a href = "http://localhost:8888?host" className="link"><div id="hostButton"> Host </div></a>
-
-          <a href = "http://localhost:8888?guest" className="link"><div id="guestButton"> Guest </div></a>
-
-
+const Home = () => (
+  <div className="main">
+    <div className="title">
+      Juke Jam
+    </div>
+    <div className="about">
+      Juke Jam is a web application that allows users to easily receive recommendations
+       for music at social events, and effortlessly add the suggestions to their party playlist.
+    </div>
+    <div className="center">
+      <a href="/about" className="goToWebsite">
+        Click here to learn more.
+      </a>
+    </div>
+    <a href="http://localhost:8888?host" className="link">
+      <div id="hostButton">
+        Host
       </div>
-    );
-  }
-}
+    </a>
+    <a href="http://localhost:8888?guest" className="link">
+      <div id="guestButton">
+        Guest
+      </div>
+    </a>
+  </div>
+);
 
-export default Home
+export default Home;
