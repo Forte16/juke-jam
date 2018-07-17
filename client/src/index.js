2227cdcc67f1c536690a59f1e3cdbb322973fb3d
Alexandro Forte <forte.alexandro@gmail.com>
2018-07-17 18:04:10 -0400

    clean up

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Host from './Host';
 import Home from './Home';
 import Guest from './Guest';
 import About from './About';
 import registerServiceWorker from './registerServiceWorker';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
 ReactDOM.render(
   <Router>
-      <div>
-        <Route exact path="/" component={Home} />
-        <Route path="/host" component={Host} />
-        <Route path="/guest" component={Guest} />
-        <Route path="/about" component={About} />
-      </div>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/host" component={Host} />
+      <Route path="/guest" component={Guest} />
+      <Route path="/about" component={About} />
+    </div>
   </Router>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
 registerServiceWorker();
