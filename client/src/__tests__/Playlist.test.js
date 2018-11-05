a19f6bd919f799379e609d74294090059d3557ce
Alexandro Forte <forte.alexandro@gmail.com>
2018-11-04 23:36:07 -0500

    remove

--- client/src/__tests__/Playlist.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Playlist from '../presentational/Playlist';
-
-it('should be selectable by class "playlist"', function () {
-  expect(shallow(<Playlist />).is('.playlist')).toBe(true);
-});
